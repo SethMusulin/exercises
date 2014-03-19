543c2cf834e34983bb993bbce43f9e82b3fff747
Seth Musulin <sethmusulin@gmail.com>
2014-03-19 16:23:10 -0600

    New test to check single word non palindrome
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- palindrome/spec/palindrome_spec.rb
+++ palindrome/spec/palindrome_spec.rb
@@ -1,11 +1,17 @@
 require "palindrome"
 
 describe Palindrome do
-  it "Returns true if the single word passed to the method is a palindrome" do
+  it "returns true if the single word passed to the method is a palindrome" do
     test = Palindrome.new
     expected = true
     actual = test.is_palindrome?("radar")
     expect(actual).to eq expected
   end
 
+  it "returns false if the single word passed to the method is not a palindrome" do
+    test = Palindrome.new
+    expected = false
+    actual = test.is_palindrome?("axmurderer")
+    expect(actual).to eq expected
+  end
 end
