05151bb11cabaa53a30daea297eb222e69a49e91
Seth Musulin <sethmusulin@gmail.com>
2014-03-20 00:46:01 -0600

    refactored with private method and ensured that palindome returns true if string contains commas, dashes and spaces.
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- palindrome/lib/palindrome.rb
+++ palindrome/lib/palindrome.rb
@@ -1,6 +1,11 @@
 class Palindrome
   def is_palindrome?(word)
+    word = remove_spaces_commas_and_dashes_and_caps(word)
     word == word.reverse
   end
 
+  private
+  def remove_spaces_commas_and_dashes_and_caps(word)
+    word = word.downcase.delete(' ').delete(',').delete('-')
+  end
 end
--- palindrome/spec/palindrome_spec.rb
+++ palindrome/spec/palindrome_spec.rb
@@ -14,4 +14,19 @@ describe Palindrome do
     actual = test.is_palindrome?("axmurderer")
     expect(actual).to eq expected
   end
+
+  it "returns true if the multi-word string passed in is a palindrome" do
+    test = Palindrome.new
+    expected = true
+    actual = test.is_palindrome?("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama")
+    expect(actual).to eq expected
+  end
+
+  it "returns true if the string with special characters is a palindrome" do
+    test = Palindrome.new
+    expected = true
+    actual = test.is_palindrome?("@foooof@")
+    expect(actual).to eq expected
+  end
+
 end
